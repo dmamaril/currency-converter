cf290dd83a36b6a0e775d62d7264655724e55b8f
Don Mamaril <mamarildon@gmail.com>
2014-08-26 18:13:57 -0700

    Integrate Hogan-Express

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var bodyParser      = require('body-parser');
 var morgan          = require('morgan');
 var flash           = require('connect-flash');
 var cookieParser    = require('cookie-parser');
+var path            = require('path');
 
 var sessionKey      = require('./app/config/secret.js');
 var port            = process.env.PORT || 8080;
@@ -21,8 +22,13 @@ app.use(cookieParser());                    // read cookies (needed for auth)
 app.use(bodyParser());                      // get information from html forms
 app.use(methodOverride());                  // simulate DELETE and PUT
 app.use(flash());                           // use connect-flash for flash messages stored in session
-
 app.use(session({ secret: sessionKey }));   // session secret
+
+/* ==== TEMPLATE ==== */
+app.set('views', path.join(__dirname + '/public', 'templates'));
+app.set('view engine', 'html');
+app.set('layout', 'layout');
+app.engine('html', require('hogan-express'));
 
 
 require('./app/routes/routes.js')(app);     // routes
