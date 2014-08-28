627bcd277405043ce4ce3961e713321425eecd0d
Don Mamaril <mamarildon@gmail.com>
2014-08-27 23:33:59 -0700

    Unrequire sessionKey

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var flash           = require('connect-flash');
 var cookieParser    = require('cookie-parser');
 var path            = require('path');
 
-var sessionKey      = require('./app/config/secret.js');
 var port            = process.env.PORT || 8080;
 
 /* ==== TEMPLATE ==== */
@@ -29,7 +28,7 @@ app.use(cookieParser());                    // read cookies (needed for auth)
 app.use(bodyParser());                      // get information from html forms
 app.use(methodOverride());                  // simulate DELETE and PUT
 app.use(flash());                           // use connect-flash for flash messages stored in session
-app.use(session({ secret: sessionKey }));   // session secret
+app.use(session({ secret: 'shhh' }));       // session secret
 
 require('./app/routes/routes.js')(app);     // routes
 
