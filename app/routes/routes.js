346a23c5d1e5fed2c5c11a1afeaf223f082cc3b4
Don Mamaril <mamarildon@gmail.com>
2014-08-27 19:00:15 -0700

    Complete /paypal/conversionRate

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,16 +7,10 @@ module.exports = function (app) {
     res.redirect('/paypal/activity');
   })
 
-  /* === DISPLAY TRANSACTION HISTORY === */
   app.get('/paypal/activity', function (req, res) {
     res.render('transactions', transactions)
   });
 
-  /* input: Amount, the currency code for the amount, 
-   * the currency code it needs to be converted to
-   *
-   * returns: the amount, the currency code for the amount, 
-   * the currency symbol */
   app.post('/paypal/currencyConversion', function (req, res) {
     var input = {};
     input.amount      = req.body.amount;
@@ -30,29 +24,27 @@ module.exports = function (app) {
         res.send(200, convertedCurrency);
       })
       .catch(function (err) {
-        // The server encountered an unexpected condition which prevented it from fulfilling the request.
         res.send(500, "Error converting currency");
       });
   });
 
-  /* input: CurrencyCode to convert from, Currency code to convert to 
-   *
-   * returns: the conversion rate */
   app.post('/paypal/conversionRate', function (req, res) {
-    var input = {};
+    var convertKey  = req.body.convertFrom + '_' + req.body.convertTo;
+    var result      = {};
+    var input       = {};
     input.convertFrom = req.body.convertFrom;
     input.convertTo   = req.body.convertTo;
 
     cc.rates(input)
-      .then(function (convertedRates) {
-        res.send(200, convertedRates);
+      .then(function (conversionRate) {
+        result[convertKey] = conversionRate;
+        res.send(200, result);
       })
       .catch(function (err) {
         res.send(500, "Error calculating conversion rate.");
       });
   });
 
-  /* ==== REDIRECT WILDCARDS ==== */
   app.get('*', function (req, res) {
     res.redirect('/paypal/activity');
   });
