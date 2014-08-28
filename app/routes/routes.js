4e68dd17492641337e9dae4839162f788e2b9af1
Don Mamaril <mamarildon@gmail.com>
2014-08-27 21:48:04 -0700

    Verify user input

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -16,33 +16,42 @@ module.exports = function (app) {
     input.amount      = req.body.amount;
     input.convertFrom = req.body.convertFrom;
     input.convertTo   = req.body.convertTo;
-    input.options     = req.body.option;
+    input.local       = req.body.local;
 
-    cc.convert(input)
-      .then(function (convertedCurrency) {
-        console.log(convertedCurrency);
-        res.send(200, convertedCurrency);
-      })
-      .catch(function (err) {
-        res.send(500, "Error converting currency");
-      });
+    if (cc.verifyInput(input)) {
+      cc.convert(input)
+        .then(function (convertedCurrency) {
+          res.send(200, convertedCurrency);
+        })
+        .catch(function (err) {
+          res.send(500, "Error converting currency");
+        });
+    } else {
+      res.send(400, "Invalid input");
+    }
   });
 
   app.post('/paypal/conversionRate', function (req, res) {
     var convertKey  = req.body.convertFrom + '_' + req.body.convertTo;
     var result      = {};
     var input       = {};
+
     input.convertFrom = req.body.convertFrom;
     input.convertTo   = req.body.convertTo;
+    input.local       = req.body.local;
 
-    cc.rates(input)
-      .then(function (conversionRate) {
-        result[convertKey] = conversionRate;
-        res.send(200, result);
-      })
-      .catch(function (err) {
-        res.send(500, "Error calculating conversion rate.");
-      });
+    if (cc.verifyInput(input)) {
+      cc.rates(input)
+        .then(function (conversionRate) {
+          result[convertKey] = conversionRate;
+          res.send(200, result);
+        })
+        .catch(function (err) {
+          res.send(500, "Error calculating conversion rate.");
+        });
+      } else {
+        res.send(400, "Invalid input");
+      }
   });
 
   app.get('*', function (req, res) {
